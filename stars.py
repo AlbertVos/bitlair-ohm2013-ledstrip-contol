dd8ec7707b71f126e4a3f4485edd2f0cb1c224fe
rogue-bitlair <roguetechnix@gmail.com>
2014-05-05 17:29:30 +0200

    .

--- stars.py
+++ stars.py
@@ -9,7 +9,7 @@ class Stars1(Effect):
 
   def __init__(self, strip2D):
     super(Stars1, self).__init__(strip2D);
-    self.getColor = self.getColor1
+    self.getColor = self.getColor3
 
   def step(self, count):
     if (count % 5) != 0:
@@ -26,16 +26,24 @@ class Stars1(Effect):
     return [random.randint(0, 255), random.randint(0, 255) \
       , random.randint(0, 255)];
 
+  rainbow = [ \
+    [255, 0, 0], [255, 96, 0], [255, 255, 0], \
+    [0, 255, 0], [0, 255, 255], \
+    [0, 0, 255], [255, 0, 255]];
+
+  def getColor3(self):
+    return self.rainbow[random.randint(0, len(self.rainbow) - 1)];
+
 
 class Stars2(Effect):
-  numstar = 2;
+  numstar = 4;
   stars = [];
 
   def __init__(self, strip2D):
     super(Stars2, self).__init__(strip2D);
-    self.getColor = self.getColor2
+    self.getColor = self.getColor3
     self.stars = [[random.randint(0, 6), random.randint(0, 20) \
-      , random.randint(-1, 1), random.randint(-2, 2), self.getColor(), 0] \
+      , random.randint(-1, 1), -1, self.getColor(), 0] \
       for each in range(self.numstar)];
 
   def step(self, count):
@@ -54,7 +62,7 @@ class Stars2(Effect):
         pass;
       else:
         if abs(vx) == 2 or abs(vy) == 2:
-          self.strip2D.set(x + vx/2, y + vy/2, c);
+          self.strip2D.set(x + vx / 2, y + vy / 2, c);
         x = x + vx;
         y = y + vy;
         self.stars[i][0] = x;
@@ -62,7 +70,7 @@ class Stars2(Effect):
         self.strip2D.set(x, y, c);
       if (y < 0) or (y >= 21) or (life >= 10):
         self.stars[i] = [random.randint(0, 6), random.randint(0, 20) \
-          , random.randint(-2, 2), random.randint(-2, 2), self.getColor(), 0]
+          , random.randint(-1, 1), -1, self.getColor(), 0]
 
   def getColor1(self):
     return [255, 255, 255];
@@ -70,6 +78,14 @@ class Stars2(Effect):
   def getColor2(self):
     return [random.randint(0, 255), random.randint(0, 255) \
       , random.randint(0, 255)];
+
+  rainbow = [ \
+    [255, 0, 0], [255, 96, 0], [255, 255, 0], \
+    [0, 255, 0], [0, 255, 255], \
+    [0, 0, 255], [255, 0, 255]];
+
+  def getColor3(self):
+    return self.rainbow[random.randint(0, len(self.rainbow) - 1)];
 
 
 if __name__ == "__main__":
